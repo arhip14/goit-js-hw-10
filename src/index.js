717f0b417b0a7bdb477f4ba22a252f7ba64409c2
arhip14 <kruvdyk1503@gmail.com>
2023-07-11 11:15:53 +0300

    initial commit 2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-const catInfo = document.createElement("div");
-catInfo.id = "cat-info";
-catInfo.style.display = "none";
+const catInfo = `
+  <div id="cat-info" style="display: none;">
+    <img id="cat-image" alt="Cat Image">
+    <h2 id="cat-breed">Breed: </h2>
+    <p id="cat-description">Description: </p>
+    <p id="cat-temperament">Temperament: </p>
+  </div>
+`;
 
-const catImage = document.createElement("img");
-catImage.id = "cat-image";
-catImage.alt = "Cat Image";
-
-const catBreed = document.createElement("h2");
-catBreed.id = "cat-breed";
-catBreed.textContent = "Breed: ";
-
-const catDescription = document.createElement("p");
-catDescription.id = "cat-description";
-catDescription.textContent = "Description: ";
-
-const catTemperament = document.createElement("p");
-catTemperament.id = "cat-temperament";
-catTemperament.textContent = "Temperament: ";
-
-catInfo.appendChild(catImage);
-catInfo.appendChild(catBreed);
-catInfo.appendChild(catDescription);
-catInfo.appendChild(catTemperament);
-
-document.body.appendChild(catInfo);
+document.body.insertAdjacentHTML("beforeend", catInfo);
