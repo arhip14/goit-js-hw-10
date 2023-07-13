3c18fe05ee3362867e57d35ea3f8560bcbeae47f
arhip14 <81581559+arhip14@users.noreply.github.com>
2023-07-13 08:38:08 +0000

    Deploying to gh-pages from @ arhip14/goit-js-hw-10@eb47e936e6776d7645e780b81d5d0e32bec92a9f 🚀

---
+++ index.735708b3.js
@@ -0,0 +1,2 @@
+var catInfo='\n\n    <div id="cat-info" style="display: none;">\n      <img id="cat-image" alt="Cat Image">\n      <h2 id="cat-breed">Breed: </h2>\n      <p id="cat-description">Description: </p>\n      <p id="cat-temperament">Temperament: </p>\n    </div>\n';document.body.insertAdjacentHTML("beforeend",catInfo);
+//# sourceMappingURL=index.735708b3.js.map
