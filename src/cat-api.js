f1f6c8cb1e111930f05250f5c5359b319ee1227c
arhip14 <kruvdyk1503@gmail.com>
2023-07-14 11:23:05 +0300

    initial commit6

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,19 +1,25 @@
+
 import axios from "axios";
+import Notiflix from "notiflix";
 
 axios.defaults.headers.common["x-api-key"] = "live_xi6ut4IHugzyv9uYHHsYP2Wmh4KO3WaWT1cxm3uIKAYDyAUNuvqR15zLb5hIc2kr";
 
 export function fetchBreeds() {
-  return axios.get("https://api.thecatapi.com/v1/breeds")
+  return axios
+    .get("https://api.thecatapi.com/v1/breeds")
     .then(response => response.data)
     .catch(error => {
+      Notiflix.Notify.failure(`Failed to fetch cat breeds: ${error.message}`);
       throw new Error(`Failed to fetch cat breeds: ${error.message}`);
     });
 }
 
 export function fetchCatByBreed(breedId) {
-  return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(response => response.data)
     .catch(error => {
+      Notiflix.Notify.failure(`Failed to fetch cat by breed: ${error.message}`);
       throw new Error(`Failed to fetch cat by breed: ${error.message}`);
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,77 @@
-import { populateBreeds } from "./app.js";
+import Notiflix from "notiflix";
+import SlimSelect from "slim-select";
+import "slim-select/dist/slimselect.css";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
+function showLoader() {
+  loader.style.display = "block";
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-  populateBreeds();
+function hideLoader() {
+  loader.style.display = "none";
+}
+
+function showCatInfo() {
+  catInfo.style.display = "block";
+}
+
+function hideCatInfo() {
+  catInfo.style.display = "none";
+}
+
+const breedSelect = document.querySelector(".breed-select");
+const catInfo = document.querySelector(".cat-info");
+const loader = document.querySelector(".loader");
+
+catInfo.style.display = "none";
+loader.style.display = "none";
+
+fetchBreeds()
+  .then((breeds) => {
+    breeds.forEach((breed) => {
+      const option = document.createElement("option");
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
+    });
+
+    new SlimSelect({
+      select: ".breed-select",
+      placeholder: "Choose cat breed :)",
+    });
+  })
+  .catch((error) => {
+    Notiflix.Notify.failure(error);
+  });
+
+breedSelect.addEventListener("change", () => {
+  const breedId = breedSelect.value;
+  showLoader();
+  hideCatInfo();
+
+  fetchCatByBreed(breedId)
+    .then((catData) => {
+      hideLoader();
+      if (catData.length === 0) {
+        return Notiflix.Notify.warning("Sorry, nothing was found :(");
+      }
+
+      showCatInfo();
+      const imageUrl = catData[0].url;
+      const catDescr = catData[0].breeds[0].description;
+      const catTemp = catData[0].breeds[0].temperament;
+
+      const markup = `
+        <img class="img-cat" src="${imageUrl}">
+        <div class="inf">
+        <p class="cat-inf"><a>Description:</a>${catDescr}</p>
+        <p class="cat-inf"><a>Temperament:</a>${catTemp}</p>
+        </div>
+      `;
+      catInfo.innerHTML = markup;
+    })
+    .catch((error) => {
+      hideLoader();
+      Notiflix.Notify.failure(error);
+    });
 });
 
-const catInfo = `
-
-  <div class="container">
-    <h1>Cat Breeds</h1>
-
-    <div class="loader-wrapper">
-      <div class="cssload-loader"></div>
-    </div>
-
-    <div class="error-wrapper"></div>
-
-    <select id="breed-select"></select>
-  </div>
-
-  <div id="cat-info" style="display: none;">
-    <img id="cat-image" alt="Cat Image">
-    <h2 id="cat-breed">Breed: </h2>
-    <p id="cat-description">Description: </p>
-    <p id="cat-temperament">Temperament: </p>
-  </div>
-
-`;
-
-document.body.insertAdjacentHTML("beforeend", catInfo);
